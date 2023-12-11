e5cb7bea646447018403668ac17f045ee0a511a8
Shahzod <sh.sh.abdinazarov@gmail.com>
2023-12-11 17:51:11 +0500

    Unused libraries are removed!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "org.hamroh.qazo"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.3"
+        versionCode = 4
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -47,40 +47,44 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.2")
-    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.2")
-
-    // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-
-    // Coroutine Lifecycle Scopes
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-
-    // Dagger - Hilt
-    implementation("com.google.dagger:hilt-android:2.48.1")
-    // kapt("com.google.dagger:hilt-compiler:2.48.1")
-    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
-
-    // Chucker
-    debugImplementation("com.github.chuckerteam.chucker:library:4.0.0")
-    releaseImplementation("com.github.chuckerteam.chucker:library-no-op:4.0.0")
+    // Paging
+    implementation("androidx.paging:paging-runtime-ktx:3.2.1")
 
     // Navigation
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
 
-    // Paging
-    implementation("androidx.paging:paging-runtime-ktx:3.2.1")
+    /*
+        implementation("androidx.legacy:legacy-support-v4:1.0.0")
+        implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+
+        // Retrofit
+        implementation("com.squareup.retrofit2:retrofit:2.9.0")
+        implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+        implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.2")
+        implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.2")
+
+        // Coroutines
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+        // Coroutine Lifecycle Scopes
+        implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+        implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+
+        // Dagger - Hilt
+        implementation("com.google.dagger:hilt-android:2.48.1")
+        // kapt("com.google.dagger:hilt-compiler:2.48.1")
+        implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+
+        // Chucker
+        debugImplementation("com.github.chuckerteam.chucker:library:4.0.0")
+        releaseImplementation("com.github.chuckerteam.chucker:library-no-op:4.0.0")
+
+        */
 
 }
--- app/src/main/java/org/hamroh/qazo/ui/main/DayViewModel.kt
+++ app/src/main/java/org/hamroh/qazo/ui/main/DayViewModel.kt
@@ -10,4 +10,5 @@ import androidx.paging.liveData
 class DayViewModel : ViewModel() {
 
     fun getList() = Pager(PagingConfig(pageSize = 10)) { DayPaging() }.liveData.cachedIn(viewModelScope)
+
 }
