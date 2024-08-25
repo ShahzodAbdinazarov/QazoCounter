727c2f6f195ba2283b70d18e5f13b1b360e8bda5
Shahzod <sh.sh.abdinazarov@gmail.com>
2024-08-26 03:11:46 +0500

    - Upgrade version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "org.hamroh.qazo"
         minSdk = 24
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.0.9"
+        versionCode = 10
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
